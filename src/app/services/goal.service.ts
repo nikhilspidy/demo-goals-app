a75ec67bfc2321fa04ef13bf2ce1a3e72fceebfd
Sam Orgill <ssorgill@icloud.com>
2018-07-29 15:59:46 +0100

    cors

--- src/app/services/goal.service.ts
+++ src/app/services/goal.service.ts
@@ -7,7 +7,7 @@ import {catchError, tap} from 'rxjs/operators';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/jsonp'
   })
 };
 
